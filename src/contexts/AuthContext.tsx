e510510095e713c7028431ff7e672d53662718d6
Goreth Manuel <goreth.costa@mamboo.co.ao>
2025-05-05 14:53:23 +0100

    feature(login-page): Criação do contexto de autenticação :sparkles:

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,3 +1,9 @@
 import { createContext } from "react";
+import { User } from "../types/user";
+export interface AuthContextTypes {
+    user: User;
+    signin: (email: string, password: string) => Promise<boolean>;
+    signout: () => void;
+}
 
-export const AuthContext = createContext(null)
+export const AuthContext = createContext<AuthContextTypes>(null!);
