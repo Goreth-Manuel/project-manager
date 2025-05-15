a836ac1f7e839d78138a3e5b1429d85578e2d3fd
Goreth Manuel <goreth.costa@mamboo.co.ao>
2025-05-15 12:01:26 +0100

    feature(dashboard-page): Identação do código :sparkles:

--- src/contexts/AuthProvider.tsx
+++ src/contexts/AuthProvider.tsx
@@ -1,58 +1,60 @@
-import { JSX, useState } from "react"
-import { AuthContext } from "./AuthContext"
+import { JSX, useState } from "react";
+import { AuthContext } from "./AuthContext";
 import { User } from "../types/user";
 import { loginUser, registerUser } from "../api/authService";
 import { useNavigate } from "react-router-dom";
 
-export const AuthProvider = ({children}: {children: JSX.Element}) => {
-    const [user, setUser] = useState<User | null>(null);
-    const navigate = useNavigate();
+export const AuthProvider = ({ children }: { children: JSX.Element }) => {
+  const [user, setUser] = useState<User | null>(null);
+  const navigate = useNavigate();
 
-     
-    const register = async (name: string, email: string, password: string): Promise<boolean> => {
-        try {
-            await registerUser(name, email, password, () => {}); // passar uma função vazia pro navigate
-      
-            // Após registrar, faça login para obter token e preencher o contexto
-            const loginResponse = await loginUser(email, password);
-            setUser({
-              uid: loginResponse.uid,
-              email: loginResponse.email!,
-              name, // já sabemos o nome, passamos na função
-              token: loginResponse.token,
-            });
-      
-            navigate("/dashboard"); // navegar manualmente agora
-            return true;
-          } catch (error) {
-            console.error("Erro ao registrar:", error);
-            return false;
-          }
-    };
-    
-    const signin = async (email: string, password: string): Promise<boolean> => {
-        try {
-            const loginResponse = await loginUser(email, password);
-            setUser({
-              uid: loginResponse.uid,
-              email: loginResponse.email!,
-              name: "", 
-              token: loginResponse.token,
-            });
-            return true;
-          } catch (error) {
-            console.error("Erro ao fazer login:", error);
-            return false;
-          }
+  const register = async (
+    name: string,
+    email: string,
+    password: string
+  ): Promise<boolean> => {
+    try {
+      await registerUser(name, email, password, () => {}); 
+
+      const loginResponse = await loginUser(email, password);
+      setUser({
+        uid: loginResponse.uid,
+        email: loginResponse.email!,
+        name,
+        token: loginResponse.token,
+      });
+
+      navigate("/dashboard");
+      return true;
+    } catch (error) {
+      console.error("Erro ao registrar:", error);
+      return false;
     }
+  };
 
-    const signout = () => {
-        setUser(null);
+  const signin = async (email: string, password: string): Promise<boolean> => {
+    try {
+      const loginResponse = await loginUser(email, password);
+      setUser({
+        uid: loginResponse.uid,
+        email: loginResponse.email!,
+        name: "",
+        token: loginResponse.token,
+      });
+      return true;
+    } catch (error) {
+      console.error("Erro ao fazer login:", error);
+      return false;
     }
+  };
 
-    return (
-        <AuthContext.Provider value={{user, register, signin, signout}}>
-            {children}
-        </AuthContext.Provider>
-    )
-}
+  const signout = () => {
+    setUser(null);
+  };
+
+  return (
+    <AuthContext.Provider value={{ user, register, signin, signout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
