1f00e5ec6e98c0e6a6f296fbd93299c3a1bbe558
Goreth Manuel <gorethmanuel0@gmail.com>
2025-05-28 00:05:53 +0100

    Atualiza a estrutura dos cards :sparkles:

--- src/components/ProjectCard/style.ts
+++ src/components/ProjectCard/style.ts
@@ -2,9 +2,8 @@ import styled from "styled-components";
 
 export const CardContainer = styled.div`
   display: flex;
-  gap: 1rem;
-  flex-wrap: wrap;
-  margin-top: 20px;
+  justify-content: space-around;
+  margin-top: 50px;
 `;
 
 export const StyledCard = styled.div`
@@ -14,8 +13,7 @@ export const StyledCard = styled.div`
   padding: 1rem;
   border-radius: 15px;
   box-shadow: 0 4px 10px rgba(0, 0, 0, 0.1);
-  min-width: 250px;
-  flex: 1;
+  min-width: 20rem;
   height: 150px;
 
   @media (max-width: 768px) {
@@ -27,12 +25,17 @@ export const StyledCard = styled.div`
 export const IconWrapper = styled.div<{ bgColor: string }>`
   background-color: ${(props) => props.bgColor};
   color: white;
+  width: 3rem;
   padding: 10px;
+  height: 3rem;
   border-radius: 12px;
+  position: relative;
+  bottom: 4.5rem;
+  margin-right: 1rem;
+
   display: flex;
   align-items: center;
   justify-content: center;
-  margin-right: 1rem;
 
   @media (max-width: 768px) {
     margin-bottom: 10px;
